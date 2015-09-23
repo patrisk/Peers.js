df39780ed04db221f089db7b8a522f95657edab7
Haggai Eran <haggai.eran@gmail.com>
2015-09-23 21:05:44 +0300

    fix nodelint warnings

--- test/basic.js
+++ test/basic.js
@@ -1,40 +1,41 @@
-var peers = require('../')
-var test = require('tape')
+var peers = require('../');
+var test = require('tape');
 
 test('initialize', function (t) {
-    var p = new peers.Peers()
-    t.assert(p, "p is true")
-    t.end()
+  var p = new peers.Peers();
+  t.assert(p, "p is true");
+  t.end();
 });
 
 test('listen', function (t) {
-    var p = new peers.Peers({
-        'loopback' : true
-    });
-    var timeoutId;
-    var passed = false;
-    p.on("listening", function () {
-        console.log('listening event');
-        hash = new Buffer(40);
-        hash.fill(0);
-        hash[0] = 1;
-        p.announce('1111', hash);
-    });
-    p.on('peer', function (peer) {
-        console.log('discovered: ' + peer)
-        if (peer.indexOf(':1111', peer.length - 5) !== -1) {
-            passed = true;
-            t.pass('got it');
-            clearTimeout(timeoutId);
-            p.stop();
-        }
-    });
-    p.listen()
-    timeoutId = setTimeout(function() {
-        if (!passed) {
-            t.fail("no discovery");
-            p.stop();
-        }
-    }, 3000);
-    t.end()
+  var p = new peers.Peers({
+      'loopback' : true
+    }),
+    timeoutId,
+    passed = false;
+  p.on("listening", function () {
+    var hash = new Buffer(40);
+
+    console.log('listening event');
+    hash.fill(0);
+    hash[0] = 1;
+    p.announce('1111', hash);
+  });
+  p.on('peer', function (peer) {
+    console.log('discovered: ' + peer);
+    if (peer.indexOf(':1111', peer.length - 5) !== -1) {
+      passed = true;
+      t.pass('got it');
+      clearTimeout(timeoutId);
+      p.stop();
+    }
+  });
+  p.listen();
+  timeoutId = setTimeout(function () {
+    if (!passed) {
+      t.fail("no discovery");
+      p.stop();
+    }
+  }, 3000);
+  t.end();
 });
